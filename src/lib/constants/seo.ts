0ee9c74b39775ce76f5543a65ef0b673b5a74820
Ciaran <cslemon96@gmail.com>
2025-01-18 18:59:44 +0200

    chore(public): copy img, favicon + default seo json from old website

---
+++ src/lib/constants/seo.ts
@@ -0,0 +1,24 @@
+export const DEFAULT_SEO = {
+    title: "Software Engineer",
+    titleTemplate: "%s | Ciarán Slemon",
+    description:
+        "I'm Ciarán, a software engineer, marketer and designer from Cape Town.",
+    keywords:
+        "Software Engineer, Portfolio, Ciarán Slemon, Typescript, Go, Cape Town, South Africa",
+    openGraph: {
+        type: "website",
+        locale: "en",
+        url: "https://ciaran.co.za/",
+        images: [
+            {
+                url: "https://ciaran.co.za/img/logo-square.png",
+                alt: "Ciarán Slemon Design Logo",
+            },
+        ],
+    },
+    twitter: {
+        handle: "@parabyl",
+        cardType: "summary_large_image",
+    },
+};
+
