b503039e95f41d8ace770fc2d84f77f6d97d3721
Ciaran <cslemon96@gmail.com>
2025-03-15 17:25:50 +0200

    feat(deps): upgrade to tailwind v4, remove unused old files

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import mdx from "@mdx-js/rollup";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         TanStackRouterVite(),
         { enforce: "pre", ...mdx() },
         react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
