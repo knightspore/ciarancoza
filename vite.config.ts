3f77d6b3fe72c10f768e983180232555d9198bb0
Ciaran <cslemon96@gmail.com>
2025-03-15 16:54:40 +0200

    feat(vite.config.ts): add mdx support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         TanStackRouterVite(),
-        react()
+        { enforce: "pre", ...mdx() },
+        react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
     ],
 })
