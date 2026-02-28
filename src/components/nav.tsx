90168c6a430900894bbe0856f7ebebb58fd84c1a
Ciaran <cslemon96@gmail.com>
2026-02-28 17:17:43 +0200

    refactor(components/nav.tsx): remove read linkgst

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -12,18 +12,14 @@ const links = [
     linkOptions({
         to: '/art',
         label: 'Art ',
-    }),
-    // linkOptions({
-    //     to: '/read',
-    //     label: 'Read',
-    // }),
+    })
 ];
 
 export default function Nav() {
     return <nav className="flex flex-wrap gap-2 md:gap-4 uppercase text-sm lg:text-lg font-medium">
         {links.map((link) => (<Link
             key={link.label}
-            className="transition-all duration-175 flex items-center gap-1"
+            className="p-1 px-2 border-2 transition-all duration-175 flex items-center gap-1"
             activeProps={{ className: 'text-coral-500' }}
             {...link}
         >
