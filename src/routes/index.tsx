4aea5f99254b4b0ee8b24d93d06ca6a092bbb71b
Ciaran <cslemon96@gmail.com>
2026-02-28 17:14:40 +0200

    feat(read.tsx): delete and update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,15 +18,7 @@ function Homepage() {
                 I love the South African countryside and I'm fascinated by learning about art, computers, history and
                 people. I've worked as a <Link href="/projects">software engineer</Link> for
                 the past few years, and I've been <Link href="/art">making music</Link> a few
-                years longer.
-            </p>
-            <p className="mt-4">
-                I'm currently working at Tripco, home of <Link href="https://lekkeslaap.co.za">LekkeSlaap</Link>. These days,
-                You can <Link href="https://www.goodreads.com/user/show/158618240-ciar-n">see
-                    what I'm reading here</Link>, and check out what I'm working on software-wise on <Link href="https://github.com/knightspore">GitHub</Link>.
-            </p>
-            <p className="mt-4">
-
+                years longer. I'm currently working at Tripco, home of <Link href="https://lekkeslaap.co.za">LekkeSlaap</Link>.
             </p>
         </div>
     )
