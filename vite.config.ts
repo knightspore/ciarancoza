5127326bbb06e51458782d49808fbe68fd7af431
Ciaran <cslemon96@gmail.com>
2026-02-28 17:04:45 +0200

    Revert "feat(vite.config.ts): add mdx support"
    This reverts commit 3f77d6b3fe72c10f768e983180232555d9198bb0.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
@@ -9,7 +8,6 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         TanStackRouterVite(),
-        { enforce: "pre", ...mdx() },
-        react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
+        react()
     ],
 })
